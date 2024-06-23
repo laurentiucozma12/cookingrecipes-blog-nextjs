44867d218084a2399010ff660736a7b12bdb30be
Lau <laurentiucozma12@gmail.com>
2024-06-23 17:51:53 +0300

    recreate project from 0 cause tailwind wont work, but now everything works fine

---
+++ sanity/sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'tg322ijd',
+    dataset: 'production'
+  }
+})
---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,12 @@
+export default function Navbar() {
+  return (
+    <nav className='w-full relative flex items-center justify-between max-w-2xl mx-auto'>
+      <div>
+        <h1 className='text-red-500'>child1</h1>
+      </div>
+      <div>
+        <h1>child2</h1>
+      </div>
+    </nav>
+  );
+}
